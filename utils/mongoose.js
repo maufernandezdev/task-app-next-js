f316e14ec312cb0390459d7ce41a1788e90135dd
Mauricio Fernandez <mfernandezuk@hotmail.com>
2023-01-24 11:10:47 -0300

    set process env to string

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -9,7 +9,7 @@ export async function dbConnect(){
     
     if(conn.isConnected) return;
     try{
-        const db = await mongoose.connect(process.env.MONGODB_URL_ATLAS);
+        const db = await mongoose.connect(process.env['MONGODB_URL_ATLAS']);
         conn.isConnected = db.connections[0].readyState;
         console.log(`Conectado a: ${db.connection.db.databaseName}`);
     }
